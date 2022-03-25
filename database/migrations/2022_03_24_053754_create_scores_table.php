7de7212c204671cb5ca5cce8d269e25d8a8fd6cb
oga559 <coolsky.4649@gmail.com>
2022-03-25 18:52:45 +0900

    テーブル定義完了

---
+++ database/migrations/2022_03_24_053754_create_scores_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id();
+            //ユーザーのidしか登録できず、スコアにuser_idを登録できるようにする ver7以降の使用可
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
